906f0bd0e5bdae9a92173acf38283f8f7b96ce12
joshexon <158853159+joshexon@users.noreply.github.com>
2024-02-09 10:12:53 +1300

    adding if statement

--- 1.py
+++ 1.py
@@ -2,13 +2,16 @@
 repeating=True 
 
 #Taking the number and printing it
-
+#start a loop for error catching
 while repeating:
     try:
-        number=float(input("please enter a number:"))
-        repeating=False
+        number=float(input("enter a positive number: "))
+        if number >= 0:
+            repeating=False
+        else:
+            print("Politly you half witted dumbass put in a positive number or piss off")
     except ValueError:
         print("Politly you half witted dumbass put in a number or piss off")
 
 
-print("the answer is " + str(number*2))
+print(str(number) + " doubled is " + str(number*2))
