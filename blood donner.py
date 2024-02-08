8e5d2ec254f832fec31b3ddbfab106a31ddef104
joshexon <158853159+joshexon@users.noreply.github.com>
2024-02-09 10:38:06 +1300

    added error checks and comments

--- blood donner.py
+++ blood donner.py
@@ -1,6 +1,30 @@
-age = float(input("Please enter your age: "))
-weight = float(input("please enter your weight: "))
+#setting the repeats to true
+repeat=True
+repeatw=True
 
+#asking for age and checking if valid
+while repeat:
+    try:
+        age = float(input("Please enter your age: "))
+        if age > 0:
+            repeat=False
+        else:
+            print("please enter a positive number")
+    except ValueError :
+        print("Make sure you enter a number with no letters")
+    
+#asking for weight and checking if valid
+while repeatw:
+    try:
+        weight = float(input("please enter your weight: "))
+        if weight > 0:
+            repeatw = False
+        else:
+            print("Make sure you enter a positive number")
+    except ValueError :
+        print("Make sure you enter a number with no letters")
+
+#checking if user is eligible and printing if they are
 if age >= 16  and weight >= 50:
     print("You are eligible")
 else:
