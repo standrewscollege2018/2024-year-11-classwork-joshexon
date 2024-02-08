dd68ba5a5eab43fd643b02ae5c5c54cb8538f4ba
joshexon <158853159+joshexon@users.noreply.github.com>
2024-02-08 14:52:57 +1300

    adding comments

--- 1.py
+++ 1.py
@@ -1,2 +1,5 @@
+'''This number takes a number input and doubles it then prints the answer'''
+#Taking the number
 number=float(input("please enter a number:"))
+#printing the number
 print("the answer is " + str(number*2))
