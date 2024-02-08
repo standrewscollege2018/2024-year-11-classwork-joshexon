afe5ef7d9d38a2ad07ad9a3513dd89bef5074e5b
joshexon <158853159+joshexon@users.noreply.github.com>
2024-02-08 15:05:34 +1300

    allowing for letters

--- 1.py
+++ 1.py
@@ -1,5 +1,9 @@
 '''This number takes a number input and doubles it then prints the answer'''
 #Taking the number
-number=float(input("please enter a number:"))
+try:
+    number=float(input("please enter a number:"))
+    print("the answer is " + str(number*2))
+except ValueError:
+    print("Politly you half witted dumbass put in a number of piss off")
 #printing the number
-print("the answer is " + str(number*2))
+
