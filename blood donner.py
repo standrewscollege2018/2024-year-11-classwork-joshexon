e794c3301a4735c6e461fbac5afdffb61de847c9
joshexon <158853159+joshexon@users.noreply.github.com>
2024-02-09 10:26:16 +1300

    basic working

---
+++ blood donner.py
@@ -0,0 +1,9 @@
+age = float(input("Please enter your age: "))
+weight = float(input("please enter your weight: "))
+
+if age >= 16  and weight >= 50:
+    print("You are eligible")
+else:
+    print("You are not eligible")
+    
+
